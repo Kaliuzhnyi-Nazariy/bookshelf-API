273bfed55b49dd04d11cd3b7f9790333072181ce
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-02-27 23:12:04 +0200

    clean app module, connect mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,28 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users/users.service';
+import { ConfigModule } from '@nestjs/config';
+
+// import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
-import { BookController } from './book/book.controller';
+// import { BookController } from './book/book.controller';
 import { BookModule } from './book/book.module';
-import { AuthService } from './auth/auth.service';
+// import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
+// import { MongodbService } from './mongodb/mongodb.service';
+import { MongodbModule } from './mongodb/mongodb.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, BookModule, AuthModule],
-  controllers: [BookController],
-  providers: [UsersService, AuthService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
+    BookModule,
+    AuthModule,
+    MongodbModule,
+    MongooseModule.forRoot(process.env.DB_CONNECT_LINK as string),
+  ],
+  // controllers: [BookController],
+  // providers: [UsersService, AuthService, MongodbService],
 })
 export class AppModule {}
