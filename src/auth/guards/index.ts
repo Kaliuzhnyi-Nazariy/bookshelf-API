d0e31296e1398bb181379400591c7957fc5ccf7c
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-03-09 11:02:24 +0200

    guards

---
+++ src/auth/guards/index.ts
@@ -0,0 +1,3 @@
+export * from './jwt.guard';
+export * from './discord.guard';
+export * from './ultimateGuard.guard';
---
+++ src/auth/guards/ultimateGuard.guard.ts
@@ -0,0 +1,37 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+// import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class UltimateGuard implements CanActivate {
+  private JwtGuard: CanActivate;
+  private DSGuard: CanActivate;
+
+  constructor(
+    // private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {
+    this.JwtGuard = new (AuthGuard('jwt'))();
+    this.DSGuard = new (AuthGuard('discord'))();
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    try {
+      const jwtRes = await this.JwtGuard.canActivate(context);
+      if (jwtRes) return true;
+    } catch (error) {
+      // console.log(error);
+      /* empty */
+    }
+
+    try {
+      const DisRes = await this.DSGuard.canActivate(context);
+      if (DisRes) return true;
+    } catch (error) {
+      // console.log(error);
+    }
+
+    return false;
+  }
+}
