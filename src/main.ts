1e1f0af243c4b2561077810c06ecbb737f1cdd49
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-03-26 22:59:13 +0200

    add cors options and changed positions

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,19 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    // origin: '', // it is for a frontend URL
+    credentials: true,
+  });
+
+  app.use(cookieParser());
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-
-  app.use(cookieParser());
   // app.useGlobalFilters(new MongooseExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3500);
