39376572647dc4d489ef5554e04207032f37f121
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-04-13 20:24:09 +0300

    fixed tests

--- test/Book/updateBook/updateBook.successfully.test.ts
+++ test/Book/updateBook/updateBook.successfully.test.ts
@@ -15,18 +15,32 @@ export function successfullyUpdateBookTest(link: string) {
       await pactum
         .spec()
         .put(`${link}/$S{bookId}`)
+        // .inspect()
         .withBearerToken('$S{userAt}')
-        .withBody(updDTO);
-      // .inspect();
+        .withBody(updDTO)
+        .expectBody({
+          ...readyBookOne,
+          _id: '$S{bookId}',
+          owner: '$S{userId}',
+          __v: 0,
+        })
+        .expectStatus(200);
     });
 
     it('update book with token in cookies', async () => {
       await pactum
         .spec()
         .put(`${link}/$S{bookId2}`)
+        .inspect()
         .withCookies('accessToken', '$S{userAt}')
-        .withBody(updDTO2);
-      // .inspect();
+        .withBody(updDTO2)
+        .expectBody({
+          ...readyBookTwo,
+          _id: '$S{bookId2}',
+          owner: '$S{userId}',
+          __v: 0,
+        })
+        .expectStatus(200);
     });
 
     it('should return empty array (user in header)', async () => {
@@ -35,12 +49,12 @@ export function successfullyUpdateBookTest(link: string) {
         .get(link)
         .withHeaders({ Authorization: 'Bearer $S{userAt}' })
         .expectJsonLength(2)
+        // .inspect()
         .expectJsonLike([
           { ...readyBookOne, owner: '$S{userId}', __v: 0 },
           { ...readyBookTwo, owner: '$S{userId}', __v: 0 },
         ])
         .expectStatus(200);
-      // .inspect();
     });
 
     it('only title field', async () => {
@@ -51,9 +65,9 @@ export function successfullyUpdateBookTest(link: string) {
         .withBody({
           title: updDTO.title,
         })
+        .inspect()
         .expectStatus(200)
         .expectJsonLike({ ...readyBookThree, owner: '$S{userId}', __v: 0 });
-      // .inspect();
     });
 
     it('only author field', async () => {
@@ -64,9 +78,9 @@ export function successfullyUpdateBookTest(link: string) {
         .withBody({
           author: updDTO2.author,
         })
+        // .inspect()
         .expectStatus(200)
         .expectJsonLike({ ...readyBookFour, owner: '$S{userId}', __v: 0 });
-      // .inspect();
     });
 
     it('only descriptonAndOpinion field', async () => {
@@ -77,9 +91,9 @@ export function successfullyUpdateBookTest(link: string) {
         .withBody({
           descripionAndOpinion: 'How to attack your goals with 10x streingth',
         })
+        // .inspect()
         .expectStatus(200)
         .expectJsonLike({ ...readyBookFive, owner: '$S{userId}', __v: 0 });
-      // .inspect();
     });
   });
 }
