99af6c7a20b652f9c9a1488a771ed9408e997fc6
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-04-17 13:39:39 +0300

    remove link

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
+    origin: '', // it is for a frontend URL
+
     // origin: '*', // it is for a frontend URL
-    origin: process.env.DEV_FRONTEND_LINK, // it is for a frontend URL
+    // origin: process.env.DEV_FRONTEND_LINK, // it is for a frontend URL
     credentials: true,
   });
 
