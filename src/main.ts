17bee48d28fa33b9ef6dce127fa8f50024e19fec
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-04-17 13:21:02 +0300

    add every frontend link

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.DEV_FRONTEND_LINK, // it is for a frontend URL
+    origin: '*', // it is for a frontend URL
+    // origin: process.env.DEV_FRONTEND_LINK, // it is for a frontend URL
     credentials: true,
   });
 
