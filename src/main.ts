0b5e872afba8e1d129ffeacb8bdc7f872cdad348
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-05-03 14:27:06 +0300

    changed the link of frontend

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200', // it is for a frontend URL
+    origin: ['http://my-fav-bookshelf-app.netlify.app'], // it is for a frontend URL
+    // origin: 'http://my-fav-bookshelf-app.netlify.app', // it is for a frontend URL
 
     // origin: '*', // it is for a frontend URL
     // origin: process.env.DEV_FRONTEND_LINK, // it is for a frontend URL
